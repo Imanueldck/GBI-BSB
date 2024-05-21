9c587aed0d5af0cdcfaba58c4a69ff48398b5180
Imanueldck <imanueldimas0108@gmail.com>
2024-05-21 22:58:21 +0700

    add styling

--- js/script.js
+++ js/script.js
@@ -1,15 +1,1 @@
-<script>
-  const contactInfo = document.querySelector('.contact-info');
-  const offcanvasContactInfo = document.getElementById('offcanvasContactInfo');
-
-  function handleResize() {
-    if (window.innerWidth <= 991.98) {
-      offcanvasContactInfo.appendChild(contactInfo);
-    } else {
-      document.body.insertBefore(contactInfo, document.body.firstChild);
-    }
-  }
-
-  window.addEventListener('resize', handleResize);
-  document.addEventListener('DOMContentLoaded', handleResize);
-</script>
+AOS.init();
